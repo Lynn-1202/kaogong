6df924349e736a8aec32d50c0245abe9201f858c
Lynn-1202 <ldn1202@163.com>
2026-05-04 22:30:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'kaogong-v4'; // 更新版本号以触发强制刷新
+const CACHE_NAME = 'kaogong-v4-final'; // 更新版本号以触发强制刷新
 const ASSETS = [
-  'kaogong-v4.html',
+  'index.html',
   'manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
